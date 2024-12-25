ec95101361a3833fc53d079bafcc4e2abb4fd3cd
dorisanse <huyenanhbui021021@gmail.com>
2024-12-25 22:16:28 +0700

    first commit

---
+++ src/main/java/com/poly/recruiterproject/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.poly.recruiterproject.controller;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
