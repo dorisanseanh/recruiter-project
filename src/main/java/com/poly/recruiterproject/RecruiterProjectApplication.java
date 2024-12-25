2ba124e2c81bdac9706ac225ec60c623716c8992
dorisanse <huyenanhbui021021@gmail.com>
2024-12-25 22:07:03 +0700

    first commit

---
+++ src/main/java/com/poly/recruiterproject/RecruiterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.poly.recruiterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruiterProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruiterProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poly/recruiterproject/RecruiterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.recruiterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruiterProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
