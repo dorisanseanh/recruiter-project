827b2579626a527a89e629e4a15b0a7b7aa85f4d
dorisanse <huyenanhbui021021@gmail.com>
2025-03-12 22:40:12 +0700

    init: create Registration repositories and controller

--- src/main/java/com/poly/recruiterproject/entity/Users.java
+++ src/main/java/com/poly/recruiterproject/entity/Users.java
@@ -16,7 +16,7 @@ import java.util.Date;
 @AllArgsConstructor
 public class Users implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", nullable = false)
     private int userId;
     
