dafb453b19e82f54324be610d9ab4b3070ef9415
dorisanse <huyenanhbui021021@gmail.com>
2025-03-13 21:29:47 +0700

    create entity recruiter profile

--- src/main/java/com/poly/recruiterproject/entity/RecruiterProfile.java
+++ src/main/java/com/poly/recruiterproject/entity/RecruiterProfile.java
@@ -1,45 +1,130 @@
-//package com.poly.recruiterproject.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Entity
-//@Table(name = "recruiter_profile")
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class RecruiterProfile {
-//    @Id
-//    @Column(name = "user_account_id")
-//    private int userAccountId;
-//
-//    @Column(name = "city")
-//    private String city;
-//
-//    @Column(name = "company")
-//    private String company;
-//
-//    @Column(name = "country")
-//    private String country;
-//
-//    @Column(name = "first_name")
-//    private String firstName;
-//
-//    @Column(name = "last_name")
-//    private String lastName;
-//
-//    @Column(name = "profile_photo")
-//    private String profilePhoto;
-//
-//    @Column(name = "state")
-//    private String state;
-//
-//    @OneToOne
-//    @MapsId // sử dụng user_account_id làm khóa chính và khóa ngoại
-//    @JoinColumn(name = "user_account_id")
-//    private Users user;
-//}
+package com.poly.recruiterproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "recruiter_profile")
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecruiterProfile {
+    @Id
+    @Column(name = "user_account_id")
+    private int userAccountId;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "company")
+    private String company;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "profile_photo")
+    private String profilePhoto;
+
+    @Column(name = "state")
+    private String state;
+
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    private Users user;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public int getUserAccountId() {
+        return userAccountId;
+    }
+
+
+    public void setUserAccountId(int userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    @Override
+    public String toString() {
+        return "RecruiterProfile{" +
+                "userAccountId=" + userAccountId +
+                ", city='" + city + '\'' +
+                ", company='" + company + '\'' +
+                ", country='" + country + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", profilePhoto='" + profilePhoto + '\'' +
+                ", state='" + state + '\'' +
+                ", user=" + user +
+                '}';
+    }
+
+
+}
