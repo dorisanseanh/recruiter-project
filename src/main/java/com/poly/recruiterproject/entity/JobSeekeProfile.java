aad98bf05e6d8fd3a8d5f5a4948cbb04a192cfbd
dorisanse <huyenanhbui021021@gmail.com>
2025-03-13 22:44:34 +0700

    Create Profiles for Recruiters and Job Seekers

--- src/main/java/com/poly/recruiterproject/entity/JobSeekeProfile.java
+++ src/main/java/com/poly/recruiterproject/entity/JobSeekeProfile.java
@@ -1,4 +1,148 @@
-//package com.poly.recruiterproject.entity;
-//
-//public class JobSeekeProfile {
-//}
+package com.poly.recruiterproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "job_seeker_profile")
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobSeekeProfile {
+    @Id
+    @Column(name = "user_account_id")
+    private int userAccountId;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "employment_type")
+    private String employmentType;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "profile_photo", nullable = true, length = 64)
+    private String profilePhoto;
+
+    @Column(name = "resume")
+    private String resume;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "work_authorization")
+    private String workAuthorization;
+
+    @OneToMany(targetEntity = Skills.class, cascade = CascadeType.ALL, mappedBy = "jobSeekerProfile")
+    private List<Skills> skills;
+
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    @MapsId
+    private Users userId;
+
+    public int getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(int userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Users getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Users userId) {
+        this.userId = userId;
+    }
+
+    public String getEmploymentType() {
+        return employmentType;
+    }
+
+    public void setEmploymentType(String employmentType) {
+        this.employmentType = employmentType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getWorkAuthorization() {
+        return workAuthorization;
+    }
+
+    public void setWorkAuthorization(String workAuthorization) {
+        this.workAuthorization = workAuthorization;
+    }
+
+    public List<Skills> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skills> skills) {
+        this.skills = skills;
+    }
+}
--- src/main/java/com/poly/recruiterproject/entity/Skills.java
+++ src/main/java/com/poly/recruiterproject/entity/Skills.java
@@ -1,37 +1,32 @@
-//package com.poly.recruiterproject.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Entity
-//@Table(name = "skills")
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class Skills {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "id")
-//    private int skillId;
-//
-//    @Column(name = "experience_level")
-//    private String experienceLevel;
-//
-//    @Column(name = "name")
-//    private String skillName;
-//
-//    @Column(name = "years_of_experience")
-//    private String yearOfExperience;
-//
-//    @OneToOne
-//    @MapsId // sử dụng user_account_id làm khóa chính và khóa ngoại
-//    @JoinColumn(name = "user_account_id")
-//    private JobSeekeProfile jobSeekeProfile;
-//
-//
-//
-//}
+package com.poly.recruiterproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "skills")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Skills {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int skillId;
+
+    @Column(name = "experience_level")
+    private String experienceLevel;
+
+    @Column(name = "name")
+    private String skillName;
+
+    @Column(name = "years_of_experience")
+    private String yearOfExperience;
+
+    @OneToOne
+    @MapsId // sử dụng user_account_id làm khóa chính và khóa ngoại
+    @JoinColumn(name = "user_account_id")
+    private JobSeekeProfile jobSeekeProfile;
+
+
+}
