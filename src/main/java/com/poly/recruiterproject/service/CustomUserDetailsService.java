6cc7d68bb1ba2d70d925fcc82bbe9f3587529b52
dorisanse <huyenanhbui021021@gmail.com>
2025-03-14 23:38:06 +0700

    config security file

---
+++ src/main/java/com/poly/recruiterproject/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.poly.recruiterproject.service;
+
+import com.poly.recruiterproject.entity.Users;
+import com.poly.recruiterproject.repository.UsersRepository;
+import com.poly.recruiterproject.util.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
