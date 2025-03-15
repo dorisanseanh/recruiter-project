ed349a503ef693961a4cb3c645f20116aa818d6b
dorisanse <huyenanhbui021021@gmail.com>
2025-03-15 20:47:08 +0700

    custom Authentication success handler

---
+++ src/main/java/com/poly/recruiterproject/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,28 @@
+package com.poly.recruiterproject.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        UserDetails user = (UserDetails) authentication.getPrincipal();
+        String username = user.getUsername();
+        System.out.println("The username is " + username + " and has been successfully logged in");
+        boolean hasJobSeekerRole = authentication.getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("Job Seeker"));
+        boolean hasRecruiterRole = authentication.getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("Recruiter"));
+        if (hasJobSeekerRole || hasRecruiterRole) {
+            response.sendRedirect("/dashboard/");
+        }
+    }
+}
