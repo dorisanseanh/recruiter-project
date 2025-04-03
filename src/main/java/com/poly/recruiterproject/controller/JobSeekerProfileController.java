ad24095b44499b8188903fbd00b5516adb084a53
dorisanse <huyenanhbui021021@gmail.com>
2025-04-03 19:33:27 +0700

    Add support for File Upload - JobSeeker Profile Image and Resume

--- src/main/java/com/poly/recruiterproject/controller/JobSeekerProfileController.java
+++ src/main/java/com/poly/recruiterproject/controller/JobSeekerProfileController.java
@@ -6,20 +6,24 @@ import com.poly.recruiterproject.entity.Skills;
 import com.poly.recruiterproject.entity.Users;
 import com.poly.recruiterproject.repository.UsersRepository;
 import com.poly.recruiterproject.service.JobSeekerProfileService;
+import com.poly.recruiterproject.util.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -68,11 +72,36 @@ public class JobSeekerProfileController {
             jobSeekerProfile.setUser(user);
             jobSeekerProfile.setUserAccountId(user.getUserId());
         }
-        List<Skills> skills = new ArrayList<>();
+        List<Skills> skillsList = new ArrayList<>();
         model.addAttribute("profile", jobSeekerProfile);
-        model.addAttribute("skills", skills);
-        return "redirect:/dashboard/";
+        model.addAttribute("skills", skillsList);
 
+        for (Skills skills : jobSeekerProfile.getSkills()) {
+            skills.setJobSeekerProfile(jobSeekerProfile);
+        }
+        String imageName = "";
+        String resumeName = "";
+        if (!Objects.equals(image.getOriginalFilename(), "")) {
+            imageName = StringUtils.cleanPath(Objects.requireNonNull(image.getOriginalFilename()));
+            jobSeekerProfile.setProfilePhoto(imageName);
+        }
+        if (!Objects.equals(image.getOriginalFilename(), "")) {
+            resumeName = StringUtils.cleanPath(Objects.requireNonNull(pdf.getOriginalFilename()));
+            jobSeekerProfile.setResume(resumeName);
+        }
+         jobSeekerProfileService.addNew(jobSeekerProfile);
+        try {
+            String uploadDir = "photos/candidate/" + jobSeekerProfile.getUserAccountId();
+            if (!Objects.equals(image.getOriginalFilename(), "")) {
+                FileUploadUtil.saveFile(uploadDir, imageName, image);
+            }
+            if (!Objects.equals(pdf.getOriginalFilename(), "")) {
+                FileUploadUtil.saveFile(uploadDir, resumeName, pdf);
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        return "redirect:/dashboard/";
 
     }
 }
