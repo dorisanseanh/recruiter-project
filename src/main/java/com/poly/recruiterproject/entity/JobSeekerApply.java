6d65ce5440ff98689d71d299eb4730b4bfaf3aa8
dorisanse <huyenanhbui021021@gmail.com>
2025-04-08 21:17:04 +0700

    Create Entities: JobSeekerApply and JobSeekerSave

--- src/main/java/com/poly/recruiterproject/entity/JobSeekerApply.java
+++ src/main/java/com/poly/recruiterproject/entity/JobSeekerApply.java
@@ -1,12 +1,86 @@
-//package com.poly.recruiterproject.entity;
-//
-//import jakarta.persistence.*;
-//
-//@Entity
-//@Table(name = "job_seeker_apply")
-//public class JobSeekerApply {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int id;
-//
-//}
+package com.poly.recruiterproject.entity;
+
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(
+        name = "job_seeker_save", // Optional: nếu bạn muốn đặt rõ tên bảng
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "job"}) // <-- dùng tên CỘT trong DB
+        }
+)
+public class JobSeekerApply implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "apply_date")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date applyDate;
+
+    @Column(name = "cover_letter")
+    private String coverLetter;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job", referencedColumnName = "job_post_id")
+    private JobPostActivity job;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_account_id")
+    private JobSeekerProfile userId;
+
+    public JobSeekerApply() {
+    }
+
+    public JobSeekerApply(Integer id, Date applyDate, String coverLetter, JobPostActivity job, JobSeekerProfile userId) {
+        this.id = id;
+        this.applyDate = applyDate;
+        this.coverLetter = coverLetter;
+        this.job = job;
+        this.userId = userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(Date applyDate) {
+        this.applyDate = applyDate;
+    }
+
+    public String getCoverLetter() {
+        return coverLetter;
+    }
+
+    public void setCoverLetter(String coverLetter) {
+        this.coverLetter = coverLetter;
+    }
+
+    public JobPostActivity getJob() {
+        return job;
+    }
+
+    public void setJob(JobPostActivity job) {
+        this.job = job;
+    }
+
+    public JobSeekerProfile getUserId() {
+        return userId;
+    }
+
+    public void setUserId(JobSeekerProfile userId) {
+        this.userId = userId;
+    }
+}
--- src/main/java/com/poly/recruiterproject/entity/JobSeekerSave.java
+++ src/main/java/com/poly/recruiterproject/entity/JobSeekerSave.java
@@ -1,4 +1,57 @@
 package com.poly.recruiterproject.entity;
 
-//public class JobSeekerSave {
-//}
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+        name = "job_seeker_save", // Optional: nếu bạn muốn đặt rõ tên bảng
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "job"}) // <-- dùng tên CỘT trong DB
+        }
+)
+public class JobSeekerSave {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job", referencedColumnName = "job_post_id")
+    private JobPostActivity job;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_account_id")
+    private JobSeekerProfile userId;
+
+    public JobSeekerSave() {
+    }
+
+    public JobSeekerSave(int id, JobPostActivity job, JobSeekerProfile userId) {
+        this.id = id;
+        this.job = job;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public JobPostActivity getJob() {
+        return job;
+    }
+
+    public void setJob(JobPostActivity job) {
+        this.job = job;
+    }
+
+    public JobSeekerProfile getUserId() {
+        return userId;
+    }
+
+    public void setUserId(JobSeekerProfile userId) {
+        this.userId = userId;
+    }
+}
